bd2c74dae69cd9bc180012edfd0878d1fa1c4e4a
Andrisevtian30 <andriyshevtiyan2004@gmail.com>
2026-03-31 20:12:15 +0800

    Add user input for greeting in main.cpp

--- main.cpp
+++ main.cpp
@@ -3,8 +3,11 @@ using namespace std;
 
 int main() {
     string nama;
+
     cout << "Silahkan inputkan nama Anda: ";
     getline(cin, nama);
+
     cout << "Hello " << nama << endl;
+
     return 0;
 }
